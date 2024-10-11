329152639f44db7e29206ea52b201519c6908b52
bilu <103429676+LucasPires1@users.noreply.github.com>
2024-10-11 08:49:39 -0300

    .

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,14 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarinformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const paragrafo = documento.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_mundo} estãoconectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+
+cosnt container = documrnto.getElementybyId('grafics-container')
+    container.appendChild(paragrafos)
+}
+
+vizualizarinformacoesGlobais()
